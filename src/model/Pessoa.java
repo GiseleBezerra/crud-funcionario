972ed82ce34665aa554e15aa497de3d4b9768fc2
GiseleBezerra <giselealmeida.ba2001@hotmail.com>
2026-06-28 22:18:12 -0300

    Classe Pessoa

--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -1,4 +1,40 @@
 package model;
 
 public class Pessoa {
+    private String nome;
+    private String cpf;
+    private int idade;
+
+    //Construtor -> valores iniciais do objeto
+    public Pessoa(String nome, String cpf, int idade){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    //Get
+    public String getNome(){
+        return nome;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public int getIdade(){
+        return idade;
+    }
+
+    //Set
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public void setIdade(int idade){
+        this.idade = idade;
+    }
 }

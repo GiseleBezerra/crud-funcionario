57d6c16db5c561328ab7b5bfa5f9cb152d2958e6
GiseleBezerra <giselealmeida.ba2001@hotmail.com>
2026-06-28 23:13:13 -0300

    Transformação e ajustes na classe para o método Herança e seu funcionamento adequado

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,27 +1,33 @@
 package model;
 
-public class Funcionario{
+//Herança - Funcionario herda atributos e métodos de Pessoa
+public abstract class Funcionario extends Pessoa{
 
     private int id;
-    private String nome;
     private double salario;
+    private String departamento;
+    private Cargo cargo;
 
-    public Funcionario(int id, String nome, double salario){
+    public Funcionario(int id, String nome, String cpf, int idade, double salario, String departamento, Cargo cargo){
+
+        super(nome, cpf, idade); //chama o construtor da classe Pessoa
+
         this.id = id;
-        this.nome = nome;
         this.salario = salario;
+        this.departamento = departamento;
+        this.cargo = cargo;
     }
 
     public int getId() {
         return id;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
     public double getSalario() {
         return salario;
+    }
+
+    public Cargo cargo(){
+        return cargo;
     }
 
     public void setSalario(double salario) {
@@ -37,7 +43,9 @@ public class Funcionario{
     @Override
     public String toString() {
         return "ID: " + id +
-                " | Nome: " + nome +
+                " | Nome: " + getNome() +
+                " | Cargo: " + cargo +
+                " | Departamento: " + departamento +
                 " | Salário: R$ " + String.format("%.2f", salario);
     }
 }
